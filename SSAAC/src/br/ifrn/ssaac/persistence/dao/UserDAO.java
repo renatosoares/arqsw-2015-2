1967f250f0e0155efaf64c44ccb41d9bb8ae43ef
Renato Soares <renatosoarespro@hotmail.com>
2016-03-19 21:51:24 -0300

    adição de metodos nos arquivos do pacote persistence.dao

--- SSAAC/src/br/ifrn/ssaac/persistence/dao/UserDAO.java
+++ SSAAC/src/br/ifrn/ssaac/persistence/dao/UserDAO.java
@@ -1,8 +1,11 @@
 package br.ifrn.ssaac.persistence.dao;
 
 import br.ifrn.ssaac.business.model.Student;
+import br.ifrn.ssaac.business.model.Evaluator;
 
 public interface UserDAO {
 	public void addStudent(Student author);
 	public void removeStudent(Student author);
+	public void addEvaluator(Evaluator author);
+	public void removeEvaluator(Evaluator author);
 }
--- SSAAC/src/br/ifrn/ssaac/persistence/dao/UserDAOImp.java
+++ SSAAC/src/br/ifrn/ssaac/persistence/dao/UserDAOImp.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import br.ifrn.ssaac.business.model.Evaluator;
 import br.ifrn.ssaac.business.model.Student;
 import br.ifrn.ssaac.persistence.util.ConnectionFactory;
 
@@ -34,6 +35,31 @@ public class UserDAOImp implements UserDAO {
 		String sql = "DELETE FROM student as s WHERE s.id = ?";
 			PreparedStatement pstRemove = conn.prepareStatement(sql);
 			pstRemove.setLong(1, student.getId());
+			pstRemove.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void addEvaluator(Evaluator evaluator) {
+		try {
+			String sql = "INSERT INTO evaluator (name, email, formation) VALUES (?, ?, ?)";
+			PreparedStatement pstInsere = conn.prepareStatement(sql);
+			pstInsere.setString(1, evaluator.getName());
+			pstInsere.setString(2, evaluator.geteMail());
+			pstInsere.setString(3, evaluator.getCourse());
+			pstInsere.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void removeEvaluator(Evaluator evaluator) {
+		try {
+		String sql = "DELETE FROM evaluator as s WHERE s.id = ?";
+			PreparedStatement pstRemove = conn.prepareStatement(sql);
+			pstRemove.setLong(1, evaluator.getId());
 			pstRemove.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
